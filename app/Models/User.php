b4a6f89e6a8af54bfc4db15bf7e1d8a4e0f16ddc
adnrei <losev_andrei@bk.ru>
2022-03-14 22:14:10 +0300

    init commit

---
+++ app/Models/User.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int $id
+ * @property string $login
+ * @property string $password
+ * @property string $token
+ */
+
+class User extends Model
+{
+    use HasFactory;
+
+    protected $table = 'users';
+
+    const id = 'id';
+    const login = 'login';
+    const password = 'password';
+    const token = 'token';
+    const created_at = 'created_at';
+    const updated_at = 'updated_at';
+
+    public function verify(string $token): bool
+    {
+        $result = self::where(self::token, $token)->exists();
+
+        return $result;
+    }
+}
---
+++ database/migrations/2022_03_13_191627_create_messages_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table
+                ->foreignId('author_id')
+                ->references('id')
+                ->on('authors')
+            ;
+
+            $table
+                ->foreignId('ticket_id')
+                ->references('id')
+                ->on('tickets')
+            ;
+
+            $table->text('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
